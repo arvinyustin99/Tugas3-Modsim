16395cefcaa4679f4c23d37c051ad2b27d252104
arvinyustin99 <44569953+arvinyustin99@users.noreply.github.com>
2021-05-08 23:54:50 +0700

    add Server class for handling job

---
+++ ModelServer.java
@@ -0,0 +1,16 @@
+import arjuna.JavaSim.Distributions.ExponentialStream;
+import arjuna.JavaSim.Simulation.*;
+
+/**
+ * This handles object of server handling the JOB
+ */
+public class ModelServer extends SimulationProcess {
+  private ExponentialStream mean_service;
+  private boolean isActive;
+  private Job current_job;
+
+  /**
+   * 
+   */
+
+}
