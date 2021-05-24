58372e86c9b12a6ae6ba3fe1d555baac47e1ac57
arvinyustin99 <44569953+arvinyustin99@users.noreply.github.com>
2021-05-24 19:33:54 +0700

    create server class for handling task

--- ModelServer.java
+++ ModelServer.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 import arjuna.JavaSim.Distributions.ExponentialStream;
 import arjuna.JavaSim.Simulation.*;
 
@@ -6,11 +8,62 @@ import arjuna.JavaSim.Simulation.*;
  */
 public class ModelServer extends SimulationProcess {
   private ExponentialStream mean_service;
-  private boolean isActive;
   private Job current_job;
 
   /**
    * 
    */
 
+  public ModelServer(double means) {
+
+    this.mean_service = new ExponentialStream(means);
+    this.current_job = null;
+  }
+
+  public double generateServiceTime() {
+    try {
+      return this.mean_service.getNumber();
+    } catch (IOException e) {
+      return 0.0;
+    }
+  }
+
+  public void run() {
+    /*
+     * This is to measure the duration the machine is active
+     */
+    double start, end;
+    System.out.println("not even here?");
+    for (;;) {
+      
+      while (!Model.jobQ.isEmpty()) {
+        start = CurrentTime();
+        
+        this.current_job = Model.jobQ.dequeue();
+        System.out.println("Job diproses");
+        // This update the job that must be queued / in delay
+        Model.JobsInQueue += Model.jobQ.getSize();
+        
+        current_job.taken();
+        
+        try {
+          double temp = generateServiceTime();
+          Hold(temp);
+        } catch (SimulationException e) {
+        } catch (RestartException e) {
+        }
+
+        end = CurrentTime();
+        Model.ActiveTime += (start - end);
+        Model.ProcessedJobs += 1;
+        current_job.finished();
+      }
+
+      try {
+        System.out.println("masuk sini cok");
+        Cancel();
+      } catch (RestartException e){
+      }
+    }
+  }
 }
