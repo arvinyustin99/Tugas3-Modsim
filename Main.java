4b7f018ceef70a59cd6d0b5f3b7fa98be8386dee
arvinyustin99 <44569953+arvinyustin99@users.noreply.github.com>
2021-05-24 19:34:12 +0700

    driver

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
-import arjuna.JavaSim.Simulation.*;
-
 public class Main{
   public static void main(String[] args){
-    
+    Model model = new Model(40.3, 200.0);
+    model.Await();
+
+    System.exit(0);
   }
 }
